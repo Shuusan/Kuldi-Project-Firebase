53be7bc66d689443ae8fac51eb5f2eea47f99469
Shuusan <joshuaalviando@gmail.com>
2024-05-31 23:22:42 +0900

    merapihkan hasil dari video firebase CLI

---
+++ lib/components/miscellaneous_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:kuldi_firebase/others/miscellaneous/page_hello_world.dart';
+
+class MiscellaneousList extends StatelessWidget {
+  const MiscellaneousList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Divider(height: 1),
+        const Text("Miscellaneous"),
+        const Divider(height: 1),
+        SizedBox(
+          height: 60,
+          width: MediaQuery.of(context).size.width,
+          child: ListView.builder(
+
+              // Jumlah Item yang akan ditampilkan
+              itemCount: 1,
+
+              // Buat biar discrollnya horizontal
+              scrollDirection: Axis.horizontal,
+
+              // Padding untuk listview
+              padding: const EdgeInsets.all(8),
+
+              // Fungsi Build
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  width: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const PageHelloWorld();
+                              },
+                              barrierDismissible: true);
+                        },
+                        child: const Text(
+                          "Our Hello World",
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
